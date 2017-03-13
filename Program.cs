50d9d314cf6b8c90d13ee7ca4c660b527f077056
Kraisorn Arjharn <kraisorna@aol.com>
2017-03-13 17:57:22 +0700

    First local commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrogJmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.solution(2,10,3));
+            Console.ReadLine();
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int X,int Y,int D)
+        {
+            return (int)Math.Ceiling((Y - X) / (double)D);
+        }
+    }
+}
